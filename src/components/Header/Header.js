cad2db2d8d5312b53542036ae5e10bee4ead509f
Rsiesener <r.siesener91@gmail.com>
2022-03-06 19:20:54 -0600

    Header
    Created header dropdown and created a styling file.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,8 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import { Link, NavLink } from 'react-router-dom'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const authenticatedOptions = (
   <>
@@ -24,11 +26,26 @@ const alwaysOptions = (
   </>
 )
 
+const authenticatedDropdown = (
+  <>
+    <DropdownButton id='dropdown-basic-button' title='Dropdown button'>
+      <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
+      <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
+      <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
+    </DropdownButton>
+  </>
+)
+
 const Header = ({ user }) => (
-  <Navbar bg='primary' variant='dark' expand='md'>
+  <Navbar bg='secondary' variant='dark' expand='md'>
     <Container>
+      <div>
+        {user ? authenticatedDropdown : <div></div>}
+      </div>
       <Navbar.Brand>
-        <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>react-auth-template</Link>
+        <Link className='justify-content-center' to='/' style={{ color: '#FFF', textDecoration: 'none' }}>
+          Chalkboard
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
